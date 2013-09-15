a9487cc8ba1acf00783966edfb1579fb542b6d4e
iamsudip <iamsudip@programmer.net>
2013-09-15 20:44:40 +0530

    Added script which will comment thank you on the all wall posts

--- bdthankall.py
+++ bdthankall.py
@@ -1,19 +1,14 @@
-
 import fbconsole
-import json
+import urllib
 from requests import get
-
-fbconsole.AUTH_SCOPE = ['publish_actions', 'user_status']
+ 
+fbconsole.AUTH_SCOPE = ['publish_actions', 'read_stream','user_actions.news', 'publish_stream']
 fbconsole.authenticate()
 
-post_id = fbconsole.fql("SELECT post_id  FROM stream WHERE source_id=me()")
-jsonf = open('post_id.json', "w")
-jsonf.write(json.dumps(post_id))
-jsonf.close()
-     
-fbconsole.logout()
-     
+s = fbconsole.fql("SELECT post_id FROM stream WHERE source_id=me() LIMIT 100")
+print fbconsole.ACCESS_TOKEN
 
-
-
-
+for i in s:
+    print str(i['post_id'])
+    f = urllib.urlopen("https://graph.facebook.com/" + str(i['post_id']) + "/comments/?access_token="+fbconsole.ACCESS_TOKEN+"&message=Thank%20you&method=POST")
+    print f.read()
